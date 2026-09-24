52fc00b558be55e76c5e0a255b8c8274fdf5034a
Florent Girault Nicolas <fgirault@c2r6p4.42nice.fr>
2026-09-24 17:08:41 +0200

    fix : c06/ex03

--- c06/ex03/ft_sort_params.c
+++ c06/ex03/ft_sort_params.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_print_params.c                                  :+:      :+:    :+:   */
+/*   ft_sort_params.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: fgirault <fgirault@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/23 16:29:43 by fgirault          #+#    #+#             */
-/*   Updated: 2026/09/23 18:40:08 by fgirault         ###   ########.fr       */
+/*   Updated: 2026/09/24 13:18:31 by fgirault         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,7 @@ int	ft_strlen(char *str)
 
 	i = 0;
 	while (str[i] != '\0')
-	{
 		i++;
-	}
 	return (i);
 }
 
@@ -32,33 +30,45 @@ void	ft_putstr(char *str)
 	write(1, str, n);
 }
 
+int ft_strcmp(char *s1, char *s2)
+{
+    int i;
+
+    i = 0;
+    while (s1[i] != '\0' || s2[i] != '\0')
+    {
+        if (s1[i] < s2[i])
+            return (-1);
+        if (s1[i] > s2[i])
+            return (1);
+        i++;
+    }
+    return (0);
+}
+
 int	main(int argc, char **argv)
 {
 	int	i;
-	int	j;
-	int	tmp;
+	char	*tmp;
 
-	i = 2;
-	j = 1;
-	while (i < argc)
+	i = 0;
+	while (i < argc - 1)
 	{
-		while (j < i)
+		if (ft_strcmp(argv[i], argv[i + 1]) == 1)
 		{
-			if (*argv[j + 1] < *argv[j])
-			{
-				tmp = *argv[j];
-				*argv[j] = *argv[j + 1];
-				*argv[j + 1] = tmp;
-			}
-			j++;
+			tmp = argv[i + 1];
+			argv[i + 1] = argv[i];
+			argv[i] = tmp;
+			i = 0;
 		}
 		i++;
 	}
-	while (**argv != '\0')
+	i = 1;
+	while (i < argc)
 	{
-		ft_putstr(*argv);
+		ft_putstr(argv[i]);
 		write(1, "\n", 1);
-		argv++;
+		i++;
 	}
 	return (0);
 }
